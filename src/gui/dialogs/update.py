f850ced1e5231dab182ce0fd2c56891c397bee78
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2017-11-11 16:26:28 +0800

    Redesign and re-implement the 'Update' dialog
    Looks good now.

--- src/gui/dialogs/update.py
+++ src/gui/dialogs/update.py
@@ -5,8 +5,7 @@ from PyQt5.QtWidgets import (QDialog,
                              QLineEdit,
                              QPushButton,
                              QHBoxLayout,
-                             QVBoxLayout,
-                             QGridLayout)
+                             QVBoxLayout)
 
 
 class UpdateGestureDialog(QDialog):
@@ -21,45 +20,54 @@ class UpdateGestureDialog(QDialog):
 
     def _widgets(self):
 
-        self.currentLabel = QLabel()
-        self.current_abbvLineEdit = QLineEdit()
-        self.current_equivLineEdit = QLineEdit()
-        self.newLabel = QLabel()
+        self.existingLabel = QLabel()
         self.new_abbvLineEdit = QLineEdit()
         self.new_equivLineEdit = QLineEdit()
         self.okPushButton = QPushButton()
 
     def _properties(self):
 
-        self.currentLabel.setText('Current Gesture:')
-        self.current_abbvLineEdit.setPlaceholderText('abbreviation')
-        self.current_equivLineEdit.setPlaceholderText('equivalent')
-        self.newLabel.setText('New Gesture:')
+        self.existingLabel.setText('Existing Gesture to update:')
         self.new_abbvLineEdit.setPlaceholderText('abbreviation')
         self.new_equivLineEdit.setPlaceholderText('equivalent')
         self.okPushButton.setText('&OK')
+        self.okPushButton.setEnabled(False)
         self.setWindowTitle('Update Gesture')
 
     def _layout(self):
 
-        grid = QGridLayout()   # 2 x 3
-        grid.addWidget(self.currentLabel, 0, 0)
-        grid.addWidget(self.current_abbvLineEdit, 0, 1)
-        grid.addWidget(self.current_equivLineEdit, 0, 2)
-        grid.addWidget(self.newLabel, 1, 0)
-        grid.addWidget(self.new_abbvLineEdit, 1, 1)
-        grid.addWidget(self.new_equivLineEdit, 1, 2)
+        first_layer = QHBoxLayout()
+        first_layer.addWidget(self.existingLabel)
+
+        second_layer = QHBoxLayout()
+        second_layer.addWidget(self.new_abbvLineEdit)
+        second_layer.addWidget(self.new_equivLineEdit)
 
         third_layer = QHBoxLayout()
         third_layer.addStretch()
         third_layer.addWidget(self.okPushButton)
 
         stack_layers = QVBoxLayout()
-        stack_layers.addLayout(grid)
+        stack_layers.addLayout(first_layer)
+        stack_layers.addLayout(second_layer)
         stack_layers.addLayout(third_layer)
 
         self.setLayout(stack_layers)
 
     def _connections(self):
 
+        self.new_abbvLineEdit.textChanged.connect(self.on_LineEdit_textChanged)
+        self.new_equivLineEdit.textChanged.connect(self.on_LineEdit_textChanged)
         self.okPushButton.clicked.connect(self.accept)
+
+    def on_LineEdit_textChanged(self):
+
+        self.check_LineEdit()
+
+    def check_LineEdit(self) -> None:
+        """ Enable or disable self.addPushButton based on LineEdit's text content. """
+
+        if not self.new_abbvLineEdit.text() == '' and not self.new_equivLineEdit.text() == '':
+            self.okPushButton.setEnabled(True)
+        else:
+            self.okPushButton.setEnabled(False)
