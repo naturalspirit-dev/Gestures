61144c3209ba4a0627aa44f261d23db5c014988f
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2017-11-27 20:21:31 +0800

    Playing with the keyboard
    Trying to execute an app and open a website. The former seems too
    awefull to implement but the latter looks feasible to implement. Will
    try this in the next coming days.

--- src/core/gestures.py
+++ src/core/gestures.py
@@ -1,6 +1,32 @@
 import keyboard as kb
 
 
+# TEST: using keyboard.add_hotkey to execute a callable
+def open_application():
+    """ Using os.system """
+
+    import os
+    print('opening Notepad...')
+    os.system(r'C:\Windows\notepad')
+
+def open_application1():
+
+    import subprocess
+    subprocess.run([r'C:\Windows\notepad'])
+
+def open_website():
+
+    import webbrowser as wb
+    wb.open_new_tab('https://www.python.org')
+
+
+# If you want a shortcut type of gesture, use add_hotkey()
+kb.add_hotkey('ctrl+alt+shift+x', open_application1)
+
+# If you want a text type of gesture, use add_word_listener()
+kb.add_word_listener("\'python", open_website)
+
+
 class Gesture:
 
     def __init__(self):
@@ -37,3 +63,4 @@ class KeyboardGesture(Gesture):
         """ Remove existing gesture in the keyboard library. """
 
         return kb.remove_abbreviation(abbv)
+
