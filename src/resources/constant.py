3d155c5a8f00d56408f7fc1547d242d7c5499d19
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2021-03-10 00:13:22 +0800

    Bump main application version number

--- src/resources/constant.py
+++ src/resources/constant.py
@@ -2,7 +2,7 @@
 
 __appname__ = 'Gestures'
 __org__ = 'GIPSC Core Team'
-__version__ = '1.3'
+__version__ = '1.4'
 __author__ = 'mokachokokarbon'
 KEYBOARD_VERSION = '0.13.2'     # core library that powers Gestures
 
