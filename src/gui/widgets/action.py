40484da78ade6f8bb48c03b694bd97fafa9c909a
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2022-05-06 00:31:56 +0800

    Added quit function to close the application

--- src/gui/widgets/action.py
+++ src/gui/widgets/action.py
@@ -6,4 +6,9 @@ class QuitAction(QAction):
     def __init__(self, parent):
 
         super().__init__('&Quit', parent)
-        self.setShortcut('ctrl+q')
+        self.setShortcut('Ctrl+Q')
+        self.triggered.connect(self.quit)
+
+    def quit(self):
+
+        exit(0)
