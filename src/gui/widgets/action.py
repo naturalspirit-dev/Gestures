7bc778e1a2fed249c0114fa1e9a38a4a9401000c
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2022-05-06 09:01:52 +0800

    Add NewAction in FileMenu
    This will open the AddGestureDialog box

--- src/gui/widgets/action.py
+++ src/gui/widgets/action.py
@@ -1,4 +1,20 @@
 from PyQt5.QtWidgets import QAction
+from src.gui.dialogs.add import AddGestureDialog
+
+
+class NewAction(QAction):
+
+    def __init__(self, parent):
+
+        super().__init__('&New', parent)
+        self.setShortcut('Ctrl+N')
+        self.triggered.connect(self.showAddGestureDialog)
+
+    def showAddGestureDialog(self):
+
+        dialog = AddGestureDialog()
+        if dialog.exec():
+            print('add new gesture')
 
 
 class QuitAction(QAction):
--- src/gui/widgets/menu.py
+++ src/gui/widgets/menu.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMenu
-from src.gui.widgets.action import QuitAction
+from src.gui.widgets.action import (NewAction,
+                                    QuitAction)
 
 
 class FileMenu(QMenu):
@@ -7,6 +8,7 @@ class FileMenu(QMenu):
     def __init__(self, parent):
 
         super().__init__('&File', parent)
+        self.addAction(NewAction(self))
         self.addAction(QuitAction(self))
 
 
