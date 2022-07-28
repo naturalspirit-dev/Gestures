1d4892ccec8720b20c1d9fc9c1be69ab478b135f
naturalspirit-dev <natural.spirit0102dev@gmail.com>
2022-07-29 04:55:59 +0800

    Refactor main.py
    Moved setting up of related processes and windows to GesturesMainApplication class.

--- src/gui/main/main_application.py
+++ src/gui/main/main_application.py
@@ -1,3 +1,4 @@
+import ctypes
 import sys
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon
 from src.gui.windows.window import GesturesMainWindow
@@ -10,6 +11,7 @@ class GesturesMainApplication(QApplication):
     def __init__(self):
 
         super().__init__(sys.argv)
+        self._set_associated_windows_in_taskbar()
         self.setOrganizationName(__orgname__)
         self.setOrganizationDomain(__orgdomain__)
         self.setApplicationName(__appname__)
@@ -24,6 +26,12 @@ class GesturesMainApplication(QApplication):
         self.systemTray.systemTrayMenu.openAction.triggered.connect(self.window.show)
         self.systemTray.systemTrayMenu.quitAction.triggered.connect(self.closeAllWindows)
         self.systemTray.systemTrayMenu.quitAction.triggered.connect(self.quit)
+
+    def _set_associated_windows_in_taskbar(self):
+        """ Group associated processes and windows under a single taskbar button. """
+
+        app_id = f'{__orgdomain__}.{__appname__}.{__version__}'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
 
     def on_systemTray_activated(self, reason: QSystemTrayIcon):
 
--- src/main.py
+++ src/main.py
@@ -6,32 +6,10 @@
     Author: mokachokokarbon
  """
 
-import logging
-import sys
-sys.path.append('..')
 from src.gui.main.main_application import GesturesMainApplication
-
-KEYPRESS_COUNT = 0
-
-
-def configure_app_icon() -> None:
-    """ This will show the icon of Gestures in the taskbar. """
-
-    import ctypes
-    APP_ID = u'mokachokokarbon.jerobado.gesture.2-0'
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
-
-
-def key_listener(event):
-
-    global KEYPRESS_COUNT
-    if event.event_type == 'down':
-        KEYPRESS_COUNT += 1
-        logging.info(f'#{KEYPRESS_COUNT} key: {event.name}')
 
 
 if __name__ == '__main__':
 
-    configure_app_icon()
     app = GesturesMainApplication()
     app.run()
